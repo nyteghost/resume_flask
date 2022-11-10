26df25a2444000e73e8e01d8fb8e772b1cea337e
Mark Brown <mbrown@nyte.tech>
2022-11-09 22:04:09 -0500

    - added weather

--- app.py
+++ app.py
@@ -1,35 +1,11 @@
 from flask import Flask
 from flask import render_template
 from flask import url_for
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def home():
-    return render_template('home.html')
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
 
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
-
-
-@app.route('/layout/')
-def header():
-    return render_template('layout.html')
-
-
-@app.route('/resume/')
-def resume():
-    return render_template('resume.html')
-
-
-@app.route('/stats/')
-def stats():
-    return render_template('stats.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+from website import app as flask_app_1
+from weather import app as flask_app_2
+application = DispatcherMiddleware(flask_app_1, {
+    '/weather': flask_app_2
+})
